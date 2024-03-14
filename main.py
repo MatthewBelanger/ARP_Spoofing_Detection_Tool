9fcc73dc29c42654fb96db8966dca5b3830f2204
Matthew Belanger <mbela1@ocdsb.ca>
2024-03-14 09:11:41 -0400

    Saving work

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pyshark
 
 capture = pyshark.LiveCapture(interface='en0', display_filter="arp")
-for pkt in capture:
-    print(pkt)
+
+def print_arp_info(packet):
+    #print("ARP Packet: opcode="+packet.arp.opcode+", Sender MAC="+packet.arp.src.hw_mac+", Sender IP="+packet.arp.src.proto_ipv4+", Target MAC="+packet.arp.dst.hw_mac+", Target IP="+packet.arp.dst.proto_ipv4)
+    print(packet.arp.proto_ipv4)
+
+capture.apply_on_packets(print_arp_info)
+

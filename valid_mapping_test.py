6282dc5d9395d0d32f831e5028cd19758b465e25
Matthew Belanger <mbela1@ocdsb.ca>
2024-04-09 14:53:16 -0400

    Added more tests

---
+++ valid_mapping_test.py
@@ -0,0 +1,9 @@
+from scapy.all import *
+
+if __name__ == '__main__':
+
+    arp_request = ARP(op=1, pdst="192.168.1.44", hwdst="00:00:00:00:00:00", psrc="192.168.1.30", hwsrc="e8:9f:80:4f:56:97")
+    send(arp_request, iface='en0', verbose=False)
+
+    arp_response = ARP(op=2, pdst="192.168.1.30", hwdst="e8:9f:80:4f:56:97", psrc="192.168.1.44", hwsrc="5c:aa:fd:97:c8:72")
+    send(arp_response, iface='en0', verbose=False)
